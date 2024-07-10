05ce7f1405fc69d2e214009f5020b6b95868dd23
pratik-ac <pra7ik@outlook.com>
2024-07-10 16:45:19 +0545

    fourteen commit

---
+++ Node/with_db/foodmandu/connect.db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://acharyapratik63:pratik123@cluster0.7zvadme.mongodb.net/kec-b4-foodmandu?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("DB connection established...");
+  } catch (error) {
+    console.log("DB connection failed...");
+    console.log(error.message);
+    process.exit();
+  }
+};
+
+export default connectDB;
---
+++ Node/with_db/foodmandu/customer/customer.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+// set rule/schema
+const customerSchema = new mongoose.Schema({
+  email: String,
+  phoneNumber: String,
+  address: String,
+});
+
+// create table/model/collection
+const Customer = mongoose.model("Customer", customerSchema);
+
+export default Customer;
