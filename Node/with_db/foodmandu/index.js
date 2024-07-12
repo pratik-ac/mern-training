3cd19fc0f1b229083dcab412a321705ca9cb427d
pratik-ac <pra7ik@outlook.com>
2024-07-12 17:22:43 +0545

    sixteen commit

--- Node/with_db/foodmandu/index.js
+++ Node/with_db/foodmandu/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./connect.db.js";
 import customerRoutes from "./customer/customer.controller.js";
+import restaurantRoutes from "./restaurant/restaurant.controller.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ connectDB();
 
 // register routes
 app.use(customerRoutes);
+app.use("/restaurant", restaurantRoutes); // ! /restaurant then we can only use /add in controller.js
 
 // network port and server
 const PORT = 8080;
