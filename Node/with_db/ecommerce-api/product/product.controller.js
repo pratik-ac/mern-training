7ed67dc5a9720c71c0e1c16f8a37711c9d01f264
pratik-ac <pra7ik@outlook.com>
2024-07-23 17:31:02 +0545

    feat: delete product + edit product

--- Node/with_db/ecommerce-api/product/product.controller.js
+++ Node/with_db/ecommerce-api/product/product.controller.js
@@ -3,6 +3,8 @@ import { isSeller, isUser } from "../middleware/authentication.middleware.js";
 import Product from "./product.model.js";
 import validateReqBody from "../middleware/validate.req.body.js";
 import { addProductValidationSchema } from "./product.validation.js";
+import validateMongoIdFromParams from "../middleware/validate.mongo.id.js";
+import checkMongoIdsEquality from "../utils/mongo.id.equality.js";
 
 const router = express.Router();
 
@@ -33,4 +35,87 @@ router.post(
     return res.status(201).send({ message: "Product is added successfully." });
   }
 );
+
+// * delete product
+router.delete(
+  "/product/delete/:id",
+  isSeller,
+  validateMongoIdFromParams,
+  async (req, res) => {
+    // extract product id from req.params
+    const productId = req.params.id;
+
+    // find product using productId
+    const product = await Product.findById(productId);
+
+    // if not product found, throw error
+    if (!product) {
+      return res.status(404).send({ message: "Product does not exists" });
+    }
+
+    // check if loggedInUserId is owner of the product
+    // * const isProductOwner = product.sellerId.equals(req.loggedInUserId);
+    // ! equivalent code
+    const isProductOwner = checkMongoIdsEquality(
+      product.sellerId,
+      req.loggedInUserId
+    );
+
+    // if not owner, throw error
+    if (!isProductOwner) {
+      return res
+        .status(403)
+        .send({ message: "You are not owner of this product" });
+    }
+
+    // delete product
+    await Product.findByIdAndDelete(productId);
+
+    // send res
+    return res.status(200).send({ message: "Product is deleted successfully" });
+  }
+);
+
+// * edit product
+router.put(
+  "/product/edit/:id",
+  isSeller,
+  validateMongoIdFromParams,
+  validateReqBody(addProductValidationSchema),
+  async (req, res) => {
+    // extract productId from req.params
+    const productId = req.params.id;
+
+    // find product using product id
+    const product = await Product.findById(productId);
+
+    // if not product, throw error
+    if (!product) {
+      return res.status(404).send({ message: "Product does not exists" });
+    }
+
+    // check product ownership
+    const isProductOwner = checkMongoIdsEquality(
+      product.sellerId,
+      req.loggedInUserId
+    );
+
+    // if not product owner, throw error
+    if (!isProductOwner) {
+      return res
+        .status(403)
+        .send({ message: "You are not owner of this product" });
+    }
+
+    // extract new values from req.body
+    const newValues = req.body;
+
+    // edit product
+    await Product.updateOne({ _id: productId }, { $set: { ...newValues } });
+
+    // send res
+    return res.status(200).send({ message: "Product is edited successfully" });
+  }
+);
+
 export default router;
