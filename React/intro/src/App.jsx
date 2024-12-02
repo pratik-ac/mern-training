558db4ac7cecc48720e9b839a765d8920f9ec929
pratik-ac <pra7ik@outlook.com>
2024-12-02 14:38:00 +0545

    feat: initialize React project with Vite; add main components, styles, and configuration files

---
+++ React/intro/src/App.jsx
@@ -0,0 +1,18 @@
+import Animal from "./Animal";
+import Button from "./Button";
+
+const App = () => {
+  return (
+    <section>
+      <p>Hello World</p>
+
+      <a href="https://www.google.com">go to google</a>
+      <p>
+        <Button />
+      </p>
+      <Animal />
+    </section>
+  );
+};
+
+export default App;
---
+++ React/intro/src/Button.jsx
@@ -0,0 +1,13 @@
+const buttonStyle = {
+  padding: "1rem",
+  background: "black",
+  color: "white",
+  border: "none",
+  cursor: "pointer",
+};
+
+const Button = () => {
+  return <button style={buttonStyle}>send me</button>;
+};
+
+export default Button;
