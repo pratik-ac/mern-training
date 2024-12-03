018fbdd8e363ec859f7dcf1a1f96256c54912a8e
pratik-ac <pra7ik@outlook.com>
2024-12-03 14:45:34 +0545

    feat: add Person component and integrate it into App; update ESLint config to disable prop-types rule

--- React/intro/src/App.jsx
+++ React/intro/src/App.jsx
@@ -1,16 +1,27 @@
 import Animal from "./Animal";
 import Button from "./Button";
+import Person from "./Person";
 
 const App = () => {
   return (
-    <section>
-      <p>Hello World</p>
-
-      <a href="https://www.google.com">go to google</a>
-      <p>
-        <Button />
-      </p>
-      <Animal />
+    <section
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "1rem",
+        justifyContent: "center",
+        alignItem: "center",
+      }}
+    >
+      <Person
+        image="https://i.insider.com/5d09448b1568f20d376ec1cb?width=800&format=jpeg&auto=webp"
+        name="Spider-man"
+      />
+      <Person image="src/assets/superman1.webp" name="Super-man" />
+      <Person
+        image="https://images.indianexpress.com/2016/06/shaktiman-1200.jpg?w=389"
+        name="Shaktiman"
+      />
     </section>
   );
 };
