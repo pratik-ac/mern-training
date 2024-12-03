03658d684b2ed4183a88205fe117aa28878cfe7b
pratik-ac <pra7ik@outlook.com>
2024-12-03 17:09:50 +0545

    feat: add Player component and replace Person instances in App; update layout styles

--- React/intro/src/App.jsx
+++ React/intro/src/App.jsx
@@ -1,6 +1,5 @@
-import Animal from "./Animal";
-import Button from "./Button";
 import Person from "./Person";
+import Player from "./Player";
 
 const App = () => {
   return (
@@ -8,19 +7,23 @@ const App = () => {
       style={{
         display: "flex",
         flexWrap: "wrap",
-        gap: "1rem",
+        gap: "2rem",
         justifyContent: "center",
         alignItem: "center",
+        marginTop: "2rem",
       }}
     >
-      <Person
-        image="https://i.insider.com/5d09448b1568f20d376ec1cb?width=800&format=jpeg&auto=webp"
-        name="Spider-man"
+      <Player
+        image="https://pbs.twimg.com/profile_images/1660478058699329537/KsVez3gh_400x400.jpg"
+        name="Kushal Bhurtel"
       />
-      <Person image="src/assets/superman1.webp" name="Super-man" />
-      <Person
-        image="https://images.indianexpress.com/2016/06/shaktiman-1200.jpg?w=389"
-        name="Shaktiman"
+      <Player
+        image="https://wicketnepal.com/wp-content/uploads/2024/02/dipendra-airee-pro.jpg"
+        name="Dipendra Singh Airee"
+      />
+      <Player
+        image="https://www.cricnepal.com/wp-content/uploads/2023/04/Sandeep-Lamichhane-after-compleing-a-fifer-against-Qatar-in-ACC-Premier-Cup-2023.jpg"
+        name="Sandeep Lamichhane"
       />
     </section>
   );
